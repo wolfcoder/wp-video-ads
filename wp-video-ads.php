e19724521e7706018206e523e817f5f8315feded
bams <bbg2021@gmail.com>
2021-07-04 05:54:39 +0800

    Initial commit

---
+++ wp-video-ads.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Wp Video Ads
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A plugin for control video ads appearing behaviour
+Version: 1.0
+Author: bbg2021
+Author URI: http://github.com/wolfcoder
+License: GPL2
+*/
